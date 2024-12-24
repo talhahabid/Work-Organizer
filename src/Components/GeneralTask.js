a35e609d2c600f3aad3cbbcbef5a50f77e1d7653
Talhah Abid <talhahabid2@gmail.com>
2024-12-24 10:43:49 -0500

    Update GeneralTask.js

--- src/Components/GeneralTask.js
+++ src/Components/GeneralTask.js
@@ -4,8 +4,8 @@ const GeneralTask = ({ task, deleteGeneralTask, undoTaskFunc,  listType }) => {
   const { id } = task;
   return ( 
     <TaskLayout task={ task }>
-        <button onClick={() => deleteGeneralTask(id, listType)} className="btn btn-danger col-3 mb-3 ml-2">Done</button>
-        <button onClick={() => undoTaskFunc(id)} className='btn btn-warning col-3 mb-3 ml-2'>Undo</button>
+        <button onClick={() => deleteGeneralTask(id, listType)} className="btn btn-danger col-lg-3 mb-3 ml-2">Done</button>
+        <button onClick={() => undoTaskFunc(id)} className='btn btn-warning col-lg-3 mb-3 ml-2'>Undo</button>
     </TaskLayout>
    );
 }
