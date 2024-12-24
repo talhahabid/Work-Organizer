eaf5fedcfb396c7489650996ad69fd58af0f88d1
Talhah Abid <talhahabid2@gmail.com>
2024-12-24 09:58:19 -0500

    Update Crud.js

--- src/Utils/Crud.js
+++ src/Utils/Crud.js
@@ -1,58 +1,59 @@
 import { sendRequestToServer } from './SendRequestToServer';
-
+var baseURL = "https://talhahabid.pythonanywhere.com/";
 const addTaskToToDo = async (task) => {
-    const url = "http://localhost:5000/add-to-todo";
+    
+    const url = baseURL+"/add-to-todo";
     await sendRequestToServer(url, "POST", task, "todo");
 };
 
 const addTaskToCompleted = async (task) => {
-    const url = "http://localhost:5000/add-to-completed";
+    const url = baseURL+"/add-to-completed";
     await sendRequestToServer(url, "POST", task, "completed");
 };
 
 const addTaskToDeleted = async (task) => {
-    const url = "http://localhost:5000/add-to-deleted";
+    const url = baseURL+"/add-to-deleted";
     await sendRequestToServer(url, "POST", task, "deleted");
 };
 
 {/*-----------------------------------------------------------------------------------------------*/}
 
 const deleteTaskFromToDo = async (id) => {
-    const url = "http://localhost:5000/delete-from-todo";
+    const url = baseURL+"/delete-from-todo";
     await sendRequestToServer(url, "DELETE", id, "todo");
 };
 
 const deleteTaskFromCompleted = async (id) => {
-    const url = "http://localhost:5000/delete-from-completed";
+    const url = baseURL+"/delete-from-completed";
     await sendRequestToServer(url, "DELETE", id, "completed");
 };
 
 const deleteTaskFromDeleted = async (id) => {
-    const url = "http://localhost:5000/delete-from-deleted";
+    const url = baseURL+"/delete-from-deleted";
     await sendRequestToServer(url, "DELETE", id, "deleted");
 };
 
 {/*-----------------------------------------------------------------------------------------------*/}
 
 const getTasksFromToDo = async () => {
-    const url = "http://localhost:5000/get-from-todo";
+    const url = baseURL+"/get-from-todo";
     return await sendRequestToServer(url, "GET", null, "todo");
 };
 
 const getTasksFromDeleted = async () => {
-    const url = "http://localhost:5000/get-from-deleted";
+    const url = baseURL+"/get-from-deleted";
     return await sendRequestToServer(url, "GET", null, 'deleted');
 };
 
 const getTasksFromCompleted = async () => {
-    const url = "http://localhost:5000/get-from-completed";
+    const url = baseURL+"/get-from-completed";
     return await sendRequestToServer(url, "GET", null, "completed");
 };
 
 {/*-----------------------------------------------------------------------------------------------*/}
 
 const updateTaskInToDo = async (task) => {
-    const url = "http://localhost:5000/update-task-from-todo";
+    const url = baseURL+"/update-task-from-todo";
     await sendRequestToServer(url, "PUT", task, "todo")
 }
 
